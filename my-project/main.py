efbfed0c8374435fbce9f4386019905021e7ba4e
Nikhil Ramanuja <Nikhil.ramanuja.8@gmail.com>
2025-08-05 16:25:45 -0700

    commit!

--- my-project/main.py
+++ my-project/main.py
@@ -49,17 +49,17 @@ class DefaultTemplate(Scene):
         self.pause(0.1)
         self.play(robot.animate.shift(UP),arrowright.animate.shift(UP),arrowleft.animate.shift(UP))
         self.play(arrowright.animate.flip(LEFT))
-        self.play(Rotate(robot,-PI/2), Rotate(arrowleft,-PI/2,about_point=robot.get_center()), Rotate(arrowright,-PI/2,about_point=robot.get_center()),run_time = 2,rate_func = linear)
+        self.play(Rotate(robot,-PI/2), Rotate(arrowleft,-PI/2,about_point=robot.get_center()), Rotate(arrowright,-PI/2,about_point=robot.get_center()),run_time = 2)
         self.play(FadeOut(arrowleft,arrowright))
         self.pause(2)
         
-        
+        questionText = Text("???").shift(UP*3)
         p1 = robot.get_center()
         p2 = p1 + [8, 0, 0]
         p4 = np.array([0, -8, 0])  
         p3 = p4 + [0,4, 0]
         bezier = CubicBezier(p1, p2, p3, p4)
-        #self.play(Create(bezier))
+        self.play(Write(questionText))
         
         # Tracker for time along the curve
         t = ValueTracker(0)
@@ -91,23 +91,70 @@ class DefaultTemplate(Scene):
 
         # Optional: stop updating
         robot.clear_updaters()
-        self.wait()
+        self.play(FadeOut(questionText))
+        self.wait(3)
 
-        # bezier curve
-        # p1 = np.array([-3, 1, 0]) 
-        # p2 = p1 + [2, 0, 0] 
-        # d1 = Dot(point=p1).set_color(BLUE) 
-        # l1 = Line(p1, p2).set_color(BLUE) 
-        # p4 = np.array([3, -1, 0]) 
-        # p3 = p4 - [2, 0, 0] 
-        # d2 = Dot(point=p4).set_color(RED) 
-        # l2 = Line(p3, p4).set_color(RED) 
-        # l3 = Line(p2,p3).set_color(GREEN)
-        # bezier = CubicBezier(p1, p2, p3, p4)
-       
-        # bezierText = Text('Bezier Curve', font_size = 100).shift(UP*3)
+        #* bezier curve
+        p1 = np.array([-3, 1, 0]) 
+        p2 = p1 + [2, 0, 0] 
+        d1 = Dot(point=p1).set_color(BLUE) 
+        l1 = Line(p1, p2).set_color(GRAY) 
+        p4 = np.array([3, -1, 0]) 
+        p3 = p4 - [2, 0, 0] 
+        d4 = Dot(point=p4).set_color(RED) 
+        l2 = Line(p3, p4).set_color(GRAY) 
+        l3 = Line(p2,p3).set_color(GRAY)
+        d2 = Dot(point=p2).set_color(GREEN)
+        d3 = Dot(point=p3).set_color(YELLOW)
+        bezier = CubicBezier(p1, p2, p3, p4)
+
+        bezierText = Text('Bezier Curve', font_size = 100).shift(UP*3)
         
-        # self.play(Create(bezier),Create(l1),Create(l2), Create(l3), Create(d1), Create(d2), Write(bezierText),run_time = 3)
-        # self.pause(5)
+        self.play(Create(bezier),Create(l1),Create(l2), Create(l3), Create(d1), Create(d2),Create(d3),Create(d4), Write(bezierText),run_time = 3)
+        l1.add_updater(lambda x: x.become(Line(d1.get_center(), d2.get_center()).set_color(GRAY)))
+        l2.add_updater(lambda x: x.become(Line(d2.get_center(), d3.get_center()).set_color(GRAY)))
+        l3.add_updater(lambda x: x.become(Line(d3.get_center(), d4.get_center()).set_color(GRAY)))
+        self.pause(2)
+        factor = 1.5
+        self.play(ScaleInPlace(d1,factor),ScaleInPlace(d2,factor),ScaleInPlace(d3,factor),ScaleInPlace(d4,factor),run_time = 0.5)
+        self.play(ScaleInPlace(d1,1/factor),ScaleInPlace(d2,1/factor),ScaleInPlace(d3,1/factor),ScaleInPlace(d4,1/factor),run_time = 0.5)
+        self.pause(1)
+        
+        # animate moving them around (setup)
+        d2.generate_target()
+        d3.generate_target()
+        bezier.generate_target()
+        
+        #move targets, there is potentially a better way using an updater on the bezier object
+        d2.target.shift(UP+RIGHT)
+        d3.target.shift(DOWN+LEFT)
+        
+        bezier.target.become(CubicBezier(p1,d2.target.get_center(),d3.target.get_center(),p4))
+        self.play(MoveToTarget(d2),MoveToTarget(d3),MoveToTarget(bezier))
+        self.pause()
+        
+        # part 2
+        d2.target.shift(DOWN*4 + LEFT)
+        d3.target.shift(UP*4 + RIGHT)
+        
+        bezier.target.become(CubicBezier(p1,d2.target.get_center(),d3.target.get_center(),p4))
+        self.play(MoveToTarget(d2),MoveToTarget(d3),MoveToTarget(bezier))
+        self.pause()
+        self.play(FadeOut(bezierText))
+        
+        #explain end pointsa nd control
+        factor = 3
+        endpointsText = Text("End Points").shift(UP*3)
+        self.play(Write(endpointsText))
+        self.play(ScaleInPlace(d1,factor),ScaleInPlace(d4,factor),run_time = 0.5)
+        self.play(ScaleInPlace(d1,1/factor),ScaleInPlace(d4,1/factor),run_time = 0.5)
+        self.play(FadeOut(endpointsText))
+        controlpointsText = Text("Control Points").shift(UP*3)
+        self.play(Write(controlpointsText))
+        self.play(ScaleInPlace(d2,factor),ScaleInPlace(d3,factor),run_time = 0.5)
+        self.play(ScaleInPlace(d2,1/factor),ScaleInPlace(d3,1/factor),run_time = 0.5)
+        self.pause()
+        # change bezier curve
+        
         
        
